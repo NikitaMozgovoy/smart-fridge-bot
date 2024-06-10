2b122bf166a1fb3e2602ffa179d9fcd1411cd9e6
NikitaMozgovoy <nikitafeodosia@gmail.com>
2024-06-10 15:30:43 +0300

    Project Init

---
+++ src/main/java/com/mozgovoy/smart_fridge_bot/SmartFridgeBotApplication.java
@@ -0,0 +1,13 @@
+package com.mozgovoy.smart_fridge_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFridgeBotApplication.class, args);
+	}
+
+}
